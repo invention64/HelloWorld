4b34dbbcc11e0a54613b0a2fab7f017e21963b39
invention64 <jimmygottshall@gmail.com>
2021-09-09 11:42:05 -0400

    Added Button to be clicked

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,10 +2,14 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // set listener function for button
+        findViewById<Button>(R.id.button).setOnClickListener({v, -> (v as Button).text = "You Clicked the Button"})
     }
 }
